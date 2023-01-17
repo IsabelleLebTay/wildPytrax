952cbafaf35484ef89c4c54569517cd04e2b0801
Isabelle Lebeuf-Taylor <lebeufta@ualberta.ca>
2023-01-16 21:50:00 -0700

    add auth method, init api.py

---
+++ wildPytrax/api.py
@@ -0,0 +1,12 @@
+
+def wt_auth():
+    """Authenticate into WildTrax
+
+    Params:
+    -----
+
+    Usage:
+    >> Obtain Auth0 credentials using WT_USERNAME and WT_PASSWORD stored as environment variables
+    >>
+    
+    """
--- wildPytrax/utils.py
+++ wildPytrax/utils.py
@@ -4,28 +4,35 @@
 
 import requests
 import os
+import datetime as dt
 
-def wt_authorization():
-    """Login credential authorization
+def _wt_authorization():
 
-    Params:
-    -----
+    bytelist = ['0x45', '0x67', '0x32', '0x4d', '0x50', '0x56', '0x74', '0x71', '0x6b',
+             '0x66', '0x33', '0x53', '0x75', '0x4b', '0x53', '0x35', '0x75', '0x58', '0x7a', '0x50',
+             '0x39', '0x37', '0x6e', '0x78', '0x55', '0x31', '0x33', '0x5a', '0x32', '0x4b', '0x31',
+             '0x69']
 
-    Usage:
-    >>
-    >>
+    cid= ''.join([chr(int(x.encode('utf-8'), 16)) for x in bytelist])
+    url = 'https://abmi.auth0.com/oauth/token'
+
+    payload = {
+        'audience' : "http://www.wildtrax.ca",
+        'grant_type' : "password",
+        'client_id' : cid,
+        'username': os.environ['USRNAME'],
+        'password': os.environ['PWD']
+    }
+
+    token_details = requests.post(url, data=payload).json()
+
+    ## Add error messages here if authentication failed
+
+    # Time until token expires
+    now = dt.datetime.now()
+    expiry_time = now + dt.timedelta(seconds=token_details['expires_in'])
+
+    return token_details
+
+_wt_authorization()
     
-    
-    """
-    cid = id
-    request = requests.post(
-        url = "https://abmi.auth0.com/oauth/token",
-        encode = "form",
-        body = list(
-            audience = "http://www.wildtrax.ca",
-            grant_type = "password",
-            client_id = cid,
-            username = os.getenv("WT_USERNAME"),
-            password = os.getenv("WT_PASSWORD")
-            )
-        )
